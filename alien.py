b2e6b5e577c40607f923f1b4d2b8f1fe20f526e6
yunoki_902B <2049113811@qq.com>
2026-06-11 18:14:24 +0800

    demo2

---
+++ alien.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.sprite import Sprite
+import time
+
+class Alien(Sprite):
+    """表示单个敌机的类"""
+    def __init__(self, ai_game):
+        """初始化敌机并设置起始位置"""
+        super().__init__()
+        self.screen=ai_game.screen
+
+        #加载敌机图像并设置其rect属性
+        self.image=pygame.image.load('image/alien.png')
+        self.rect=self.image.get_rect()
+
+        #存储敌机的精确位置
+        self.x=float(self.rect.x)
+        self.y=float(self.rect.y)
+
+        #每个敌机最初都在屏幕左上角之外
+        self.rect.x=self.rect.width
+        self.rect.y=(-180)
+
+        #敌机速度
+        self.settings=ai_game.settings
+
+        #敌机计时器
+        self.timer=0
+
+    def update(self):
+        """敌机移动"""
+        self.timer+=1
+        if self.y<(140):
+            self.y += self.settings.alien_speed
+            self.rect.y=self.y
+        elif self.timer>160:
+            if self.x<=300:
+                self.x-=self.settings.alien_speed
+                self.rect.x=self.x
+            else:
+                self.x+=self.settings.alien_speed
+                self.rect.x=self.x
--- ship.py
+++ ship.py
@@ -7,13 +7,43 @@ class Ship:
         """初始化飞船并设置初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings=ai_game.settings
 
         # 加载飞船图像并获取其外接矩形
-        self.image = pygame.image.load('image/ship.bmp')
+        self.image = pygame.image.load('image/ship.png')
         self.rect = self.image.get_rect()
 
         # 每艘新飞船都放在屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船属性x里存储浮点数
+        self.x=float(self.rect.x)
+        self.y=float(self.rect.y)
+
+        #移动标志
+        self.moving_right=False
+        self.moving_left=False
+        self.moving_up=False
+        self.moving_down=False
+
+    def update(self):
+        """根据移动标志调整飞船位置"""
+        # 更新飞船属性的值，而不是外接矩形的属性值
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y+=self.settings.ship_speed
+            
+        if self.moving_up and self.rect.top > 0:
+            self.y-=self.settings.ship_speed
+
+        # 根据 self.x / self.y 更新 rect 对象
+        self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """在指定位置放置飞船"""
